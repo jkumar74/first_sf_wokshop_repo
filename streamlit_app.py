bb5aa43dbaac86fff83103dfa4508f7ff66be53f
jkumar74 <130192899+jkumar74@users.noreply.github.com>
2023-04-08 06:07:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamit
 
 streamit.title('First Streamit Program')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
